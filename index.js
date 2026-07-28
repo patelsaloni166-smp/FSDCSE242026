b3058871839f03b4b3706d54634ead10848a65b6
patelsaloni166-smp <patelsaloni166@gmail.com>
2026-07-28 10:08:57 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-console.log("hello");
+// console.log("hello");
+
+// const  sum=function(a,b){
+//     return a+b;
+// }
+// console.log(sum(2, 3));
+
+// const sum=(a,b)=>{return a+b}
+// console.log(sum(2,3))
+
+// IIFE-Immediate invoke function as expression
+// (()=>{
+//     console.log("Heyy...using IIFE")
+// })();
+
+
+// let a=23;
+// // console.log(typeof a);
+// if(a>20){
+//  let a=40;
+//  console.log("value inside block ="+a)
+// }
+// console.log('value outside block='+a)
+
+// let have block scope
+
+
+// Callback function
+// function sum(a,b){
+//     return a+b;
+// }
+
+// function msgWithSum(clbk,msg){
+//    const result= clbk(40,50);
+//    console.log("Hii,"+msg+" and your result="+result)
+// }
+
+// msgWithSum(sum,"Saloni");
+
+
+
+function login(error,msg)
+{
+    if(error){
+        console.log("Error is:"+error)
+    }
+    else{
+        console.log(msg)
+    }
+}
+
+function loginhandler(username,password,clbk){
+    if(username=="Saloni@166" && password=="12345"){
+    clbk(null,"login success")
+    }
+    else{
+        clbk("username or password is incorrect",null)
+    }
+}
+loginhandler("Saloni@166","123456",login)
