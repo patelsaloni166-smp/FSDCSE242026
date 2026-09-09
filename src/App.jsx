058e20357092aa88d6dbca6a8d719896f61984e6
patelsaloni166-smp <patelsaloni166@gmail.com>
2026-09-09 12:15:53 +0530

    icard commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+import ICard from './components/ICard'
+import ICardGallery from './components/ICardGallery'
+
+function App() {
+ 
+
+  return (
+    <div style={{border:"2px solid black",backgroundColor:"skyblue",height:"60vh",width:"30vw" }}> 
+      <ICardGallery/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ICard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function ICard({data}) {
+  
+  return (
+    <div style={{border:"2px solid red",backgroundColor:"black",height:"60vh",width:"30vw"}}>
+      <h2>College:{data.college}</h2>
+     <div>
+      <img src={data.pic} alt="Profile" height={200} width={200}/>
+     </div>
+      <h2>Roll Number:{data.rollNumber}</h2>
+      <h2>Name:{data.name}</h2>
+      <h2>Branch:{data.branch}</h2>
+    </div>
+  )
+}
+
+export default ICard
+
+
+
+
+
